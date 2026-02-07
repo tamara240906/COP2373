07802d177f040db9f5e3be9d46b5936b567d32a6
tamarasamokhvalova <tamara240906@gmail.com>
2026-02-07 14:55:58 -0500

    Changing the number of tickets to be sold from 20 to 10.

--- TamaraSamokhvalova_ProgrammingExercise_1.py
+++ TamaraSamokhvalova_ProgrammingExercise_1.py
@@ -17,8 +17,10 @@ def ask_for_tickets():
     Return:
         tickets(int): the number of tickets the user wants.
     '''
-
+    # Prompt the user to enter a number between 1 and 4
     tickets = int(input("How many tickets would you like to buy (1 to 4)? "))
+
+    # Return the user's input
     return tickets
 
 # Function 2: run the ticket selling program
@@ -43,20 +45,26 @@ def sell_tickets():
         None
     '''
 
-    tickets_left = 20
+    tickets_left = 10
     buyers = 0
     while tickets_left > 0:
+        # Display the currently number of tickets remaining
         print("Tickets remaining:", tickets_left)
 
+        # Ask the user for the number of tickets requested
         wanted = ask_for_tickets()
 
+        # Validate the number of tickets requested
         if wanted >= 1 and wanted <=4 and wanted <= tickets_left:
             tickets_left -= wanted
             buyers += 1
             print("Tickets remaining after purchase:", tickets_left)
+
         else:
             print("Invalid number of tickets.")
+
     print("All tickets have been sold.")
+
     print("Total number of buyers:", buyers)
 
 #Run the program
